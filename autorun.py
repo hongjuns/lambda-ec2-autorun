d9466d5d8ca6c0f1bb0514abb18a9101d6637510
hongjuns <asws12345@naver.com>
2021-09-21 15:26:56 +0900

    인스턴스 상태를 확인하여 start_instances 또는 stop_instances 함수 호출

--- autorun.py
+++ autorun.py
@@ -5,6 +5,8 @@ ec2 = boto3.client('ec2', 'ap-northeast-2')
 def lambda_handler(event, context):
     reservations = ec2.describe_instances()
     ec2_list = []
+    ec2_list_start = []
+    ec2_list_stop = []
     for attr in reservations["Reservations"]:
        for instance in attr["Instances"]:
            for tag in instance["Tags"]:
@@ -12,4 +14,13 @@ def lambda_handler(event, context):
                     ec2_list.append(instance)
 
      
+    for tagInstance in ec2_list:
+        if tagInstance["State"]["Name"] == 'running':
+            ec2_list_start.append(tagInstance["InstanceId"])
+            ec2.stop_instances(InstanceIds=ec2_list_start)
+        elif tagInstance["State"]["Name"] == 'stopped':
+            ec2_list_stop.append(tagInstance["InstanceId"])
+            ec2.start_instances(InstanceIds=ec2_list_stop)
+        else:
+            print("Nothing to see here")
 
